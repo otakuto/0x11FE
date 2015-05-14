51acc0ea1250e356e304fe8100334fb82119c4ab
otakuto <otakuto5648@gmail.com>
2015-05-14 14:59:26 +0900

    upgrade glfw

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <GL/glfw.h>
+#include <GLFW/glfw3.h>
+#include <GL/glu.h>
 #include <vector>
 #include <iostream>
 #include <cmath>
@@ -7,26 +8,37 @@
 int main()
 {
 	glfwInit();
-	glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
 
-	glfwSetWindowSizeCallback([](int w, int h)
-	{
-		glViewport(0, 0, w, h);
-		glLoadIdentity();
-		gluPerspective(90, static_cast<double>(w) / static_cast<double>(h), 1, 100);
-		gluLookAt(0, 0, 64, 0, 0, 0, 0, 1, 0);
-	});
+	int count;
+	GLFWmonitor ** monitors = glfwGetMonitors(&count);
+	GLFWmonitor * monitor = monitors[0];
+	//GLFWmonitor * monitor = glfwGetPrimaryMonitor();
+	GLFWvidmode const * mode = glfwGetVideoMode(monitor);
+	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+	GLFWwindow * window = glfwCreateWindow(mode->width, mode->height, "0x11FE", monitor, nullptr);
+    glfwMakeContextCurrent(window);
 
 	Game game;
 
-	while (glfwGetWindowParam(GLFW_OPENED))
+    while(!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
 
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glLoadIdentity();
+		gluPerspective(90, static_cast<double>(width) / static_cast<double>(height), 1, 1024);
+		gluLookAt(0, 100, 64, 0, 0, 0, 0, 1, 0);
+
 		game.run();
 		game.draw();
-		glfwSwapBuffers();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 	glfwTerminate();
 }
